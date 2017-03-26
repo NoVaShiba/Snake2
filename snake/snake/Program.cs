82f30beeadccd31a767b4319c13c3d144d91bcf2
  <v.shibalkovv.@mail.ru>
2017-03-26 19:35:09 +0400

    Create Snake

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -24,7 +24,9 @@ namespace snake
 
             //отрисовака точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
         }
     }
 }
